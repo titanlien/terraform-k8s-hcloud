4c6474c49eb44cf294773f5a6235e477e2dc4bbf
Titan Lien <titan.lien@gmail.com>
2019-09-01 17:20:17 +0200

    install calico on hcloud

--- install-calico.tf
+++ install-calico.tf
@@ -1,8 +1,8 @@
-resource "null_resource" "calico" {
+resource "null_resource" "calico-master" {
   count = "${var.calico_enabled ? 1 : 0}"
 
   connection {
-    host        = "${hcloud_server.master.*.ipv4_address}"
+    host        = hcloud_server.master.*.ipv4_address
     private_key = "${file(var.ssh_private_key)}"
   }
 
@@ -15,4 +15,23 @@ resource "null_resource" "calico" {
   }
 
   depends_on = ["hcloud_server.master"]
+}
+
+resource "null_resource" "calico-node" {
+  count = "${var.calico_enabled ? 1 : 0}"
+
+  connection {
+    host        = hcloud_server.node.*.ipv4_address
+    private_key = "${file(var.ssh_private_key)}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "kubectl apply -f https://docs.projectcalico.org/v3.2/getting-started/kubernetes/installation/hosted/etcd.yaml",
+      "kubectl apply -f https://docs.projectcalico.org/v3.2/getting-started/kubernetes/installation/rbac.yaml",
+      "kubectl apply -f https://docs.projectcalico.org/v3.2/getting-started/kubernetes/installation/hosted/calico.yaml",
+    ]
+  }
+
+  depends_on = ["hcloud_server.node"]
 }
