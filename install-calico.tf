6c692c93ddd3448e8526e3eb4e1da5a99e9dff3b
Titan Lien <titan.lien@gmail.com>
2019-09-08 18:20:07 +0200

    Remove the usless codes. Modify the variable name depend on inventory. Create a snapshot with kernel 5.12.3, docker 18.09.9 by default

--- install-calico.tf
+++ install-calico.tf
@@ -16,22 +16,3 @@ resource "null_resource" "calico-master" {
 
   depends_on = ["hcloud_server.master"]
 }
-
-resource "null_resource" "calico-node" {
-  count = "${var.calico_enabled ? 1 : 0}"
-
-  connection {
-    host        = hcloud_server.node.*.ipv4_address
-    private_key = "${file(var.ssh_private_key)}"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "kubectl apply -f https://docs.projectcalico.org/v3.2/getting-started/kubernetes/installation/hosted/etcd.yaml",
-      "kubectl apply -f https://docs.projectcalico.org/v3.2/getting-started/kubernetes/installation/rbac.yaml",
-      "kubectl apply -f https://docs.projectcalico.org/v3.2/getting-started/kubernetes/installation/hosted/calico.yaml",
-    ]
-  }
-
-  depends_on = ["hcloud_server.node"]
-}
